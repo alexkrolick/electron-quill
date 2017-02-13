9369ac8f8cbda9b77b5623bf87382e67b96b62b7
Alex Krolick <whokilledtheelectricmonk@gmail.com>
2017-02-12 17:21:55 -0800

    Add sensible default styles and use inline toolbar

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
 /** @jsx h */
-import { h, render } from 'preact'
+import { h, render, Component } from 'preact'
 import { Header, Title, Footer, Button, ButtonGroup, NavGroup } from 'preact-photon'
 import Quill from './editor.jsx'
+import { remote } from 'electron'
+const { dialog } = remote
 
 const Sidebar = () => (
-  <div className='pane pane-sm sidebar'>
+  <div className='pane pane-sm sidebar hide'>
     <NavGroup>
       <NavGroup.Title>Places</NavGroup.Title>
       <NavGroup.Item icon='home'>Home</NavGroup.Item>
@@ -12,34 +14,60 @@ const Sidebar = () => (
   </div>
 )
 
-render((
-  <div className='window'>
-    <Header>
-      <Title>React Editor</Title>
-      <div className='toolbar-actions'>
-        <ButtonGroup>
-          <Button icon='left' />
-          <Button icon='right' />
-        </ButtonGroup>
-        <Button icon='arrows-ccw' class='pull-right' />
-        <Button icon='home'>Home</Button>
-      </div>
-    </Header>
+class App extends Component {
 
-    <div className='window-content'>
-      <div className='pane-group'>
-        <Sidebar />
-        <div className='pane'>
-          <Quill placeholder={"Let's go!"} container='#editor' />
-          <div id='editor' />
+  handleClickSave = () => {
+    dialog.showSaveDialog()
+  }
+
+  handleClickOpen = () => {
+    dialog.showOpenDialog({
+      properties: ['openFile'],
+    })
+  }
+
+  render () {
+    return (
+      <div className='window'>
+        <Header>
+          <Title>React Editor</Title>
+          <div className='toolbar-actions'>
+            <ButtonGroup>
+              <Button
+                icon='floppy'
+                onClick={this.handleClickSave}
+              />
+              <Button
+                icon='folder'
+                onClick={this.handleClickOpen}
+              />
+            </ButtonGroup>
+            <Button icon='plus' class='pull-right' />
+          </div>
+        </Header>
+
+        <div className='window-content'>
+          <div className='pane-group'>
+            <Sidebar />
+            <div className='pane'>
+              <Quill
+                placeholder={"Let's go!"}
+                container='#editor'
+                theme={'bubble'}
+              />
+              <div id='editor' />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
 
-    <Footer>
-      <Title>
-        0 Words
-      </Title>
-    </Footer>
-  </div>
-), document.body)
+        <Footer>
+          <Title>
+            0 Words
+          </Title>
+        </Footer>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.body)
